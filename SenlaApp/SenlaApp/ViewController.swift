55b1fcc1fd9d0aa729dd752c8382c438f0bb90a2
zacepilkina <zacepilina02_00@mail.ru>
2022-03-19 16:57:55 +0300

    creates basic project

---
+++ SenlaApp/SenlaApp/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  SenlaApp
+//
+//  Created by Anastasia on 19.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ViewCus: UIView!
+    @IBOutlet weak var myButton: UIButton!
+    
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+    @IBAction func UIButton(_ sender: Any) {
+        print("Button did tap")
+        titleLabel.text = "App"
+        ViewCus.backgroundColor = .green
+    }
+}
+
+extension ViewController {
+    
+}
